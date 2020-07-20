3b0a49483c5c9dc03437dbc63507428874316a8a
MrunalGondhale <43275132+MrunalGondhale@users.noreply.github.com>
2020-07-20 16:13:09 +0530

    Add files via upload
    I am using basic modules in this project.Behind this project i have used simple Binary search Algorithm Concepts. I begin with response of the end user,if User type yes then number is match with random number   otherwise we have to used binary search algorithm. First of all we have to find mid and check if number is equal to the mid or not, if not then traverse left or right side of list.if number is greater than mid then low=mid+1 i.e traverse  right side of the list. If number is less than mid then high=mid-1,i.e traverse left side of the list. Again process will be same till specific number is match.I hope everyone should understand my code.I can make the use of random module and binary search algorithm including conditional statements.

---
+++ Mind-game.py
@@ -0,0 +1,20 @@
+import random as r
+def midle(low, high):
+  return r.randint(low,high)
+ans = input("Enter the y/Y if you would play or n/N  ")
+print("\n Guess the number")
+response= 'N'
+low = 1
+high = 10
+if(ans == 'Y' or ans == 'y'):
+  while(response == 'N' or response == 'n'):
+    mid = midle(low, high)
+    print("\n Your number is {}".format(mid))
+    response = input("\nEnter Y/y if is it your no. otherwise N/n ")
+    if(response == 'N' or response == 'n'):
+      a=input("\nEnter Y\y if your no is bigger Otherwise N/n")
+      if(a == 'Y' or a == 'y'):
+        low = mid + 1
+      else:
+        high = mid - 1
+print("\nThank You")
